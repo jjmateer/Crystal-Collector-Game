1e6d99901feaf8b8d71af472f918ce69a81bbee4
John Mateer <jj.mateer@live.com>
2019-07-02 21:11:55 -0700

    pseudo code for java

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,26 @@
+//Create variables
 var randomNum;
 var wins;
 var losses;
 var userScore
+//A game with 4 crystals
 
-//
+// Every crystal needs to have a random number between 1 and 12
+
+//Random number that you have to guess is between 19 and 120
+
+
+//When the player clicks on a crystal, it will add a specific amount of points to the plays total score.
+
+    //Your game will hide this amount until the player clicks a crystal.
+    
+    //When they do click one, update the player's score counter.
+
+//If the total score matches the random number, then the player wins.
+
+//The player loses if their score goes above the random number.
+
+//The game restarts whenever the player wins of loses.
+
+// Show the number of games the player wins and loses.
+
