8bb1d9208171e3a0119c6c9be9123fdfbf3bb676
John Mateer <jj.mateer@live.com>
2019-07-03 02:53:51 -0700

    Linked css stylesheet, put background colors and sized all containers, added image background to body. In java, I created a for loop that makes 4 random numbers that I will use for the crystal buttons in future.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,39 @@
 //Create variables
-var randomNum;
+var randomResult;
 var wins;
 var losses;
-var userScore
-//A game with 4 crystals
+var userScore = "";
+
+//A game with 4 crystals and a random result
+randomResult = Math.floor(Math.random() * 120 - 5);
+$("#result").html('Random Result : ' + randomResult);
+
 
 // Every crystal needs to have a random number between 1 and 12
+for (var i = 0; i < 4; i++) {
+    var random = Math.floor(Math.random() *  12);
+    console.log(random);
 
-//Random number that you have to guess is between 19 and 120
+    var crystal = $('<div>');
+    crystal.attr({
+        "class": 'crystal',
+        "data-random": random
+        
+    });
+    $(".crystals").append(crystal);
 
+
+}
 
 //When the player clicks on a crystal, it will add a specific amount of points to the plays total score.
-
-    //Your game will hide this amount until the player clicks a crystal.
+    $("crystal").click(function(){
+        
     
     //When they do click one, update the player's score counter.
+
+
+    //Create random number that you have to guess is between 19 and 120
+    });
 
 //If the total score matches the random number, then the player wins.
 
