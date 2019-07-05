115c8f4d8fd82d123a35308f05eeb54097eb5615
John Mateer <jj.mateer@live.com>
2019-07-05 15:24:07 -0700

    Instead of creating buttons and random values with a for loop, I made 4 separate variables with random result and attatched them to buttons I made in the index. I made on click functions individually for the buttons and used .text to push the user score to the page.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,45 +4,73 @@ var wins = 0;
 var losses = 0;
 var userScore = 0;
 
+
+
 //A game with 4 crystals and a random result
 
 //Create random number that you have to guess is between 19 and 120
-randomResult = Math.floor(Math.random() * 120) + 1;
+randomResult = Math.floor(Math.random() * 102 + 19);
 $("#randomNum").html('Random Number: ' + randomResult);
 
 
 // Every crystal needs to have a random number between 1 and 12
-for (var i = 0; i < 4; i++) {
-    var random = Math.floor(Math.random() * 12) + 1;
-    console.log(random);
-   
-    var crystal = $('<button>', '</button>');
 
-    crystal.attr({
-        "class": 'crystal',
-    });
+var num1 = Math.floor(Math.random() * 12 + 1);
+var num2 = Math.floor(Math.random() * 12 + 1);
+var num3 = Math.floor(Math.random() * 12 + 1);
+var num4 = Math.floor(Math.random() * 12 + 1);
 
-    $(".crystals").append(crystal);
-    $(".crystal").push(random);
-    
+//Create a reset function
+function reset() {
+    randomResult = Math.floor(Math.random() * 102 + 19);
+    $("#randomNum").html('Random Number: ' + randomResult);
+    var num1 = Math.floor(Math.random() * 12 + 1);
+    var num2 = Math.floor(Math.random() * 12 + 1);
+    var num3 = Math.floor(Math.random() * 12 + 1);
+    var num4 = Math.floor(Math.random() * 12 + 1);
+    userScore = 0;
+    console.log();
+
+
 }
 
+
+
+
 //When the player clicks on a crystal, it will add a specific amount of points to the players total score.
-$('.crystal').on("click", function() {
+$('#red').on("click", function () {
 
-userScore =  random + userScore;
-   
+    userScore = num1 + userScore;
 
-$('#totalScore2').text(userScore)
-})
+    $('#totalScore2').text(userScore)
+});
+$('#blue').on("click", function () {
 
+    userScore = num2 + userScore;
 
+    $('#totalScore2').text(userScore)
+});
+$('#white').on("click", function () {
+
+    userScore = num3 + userScore;
+
+    $('#totalScore2').text(userScore)
+});
+$('#yellow').on("click", function () {
+
+    userScore = num4 + userScore;
+
+    $('#totalScore2').text(userScore)
+});
+$('#winsText').text("Wins: " + wins)
+$('#lossesText').text("Losses: " + losses)
 //If the total score matches the random number, then the player wins.
+
 
 
 //The player loses if their score goes above the random number.
 
+
 //The game restarts whenever the player wins of loses.
 
 // Show the number of games the player wins and loses.
-
