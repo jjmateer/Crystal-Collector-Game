c0c101d2169b15b7f606b0f212dba57d33a20134
John Mateer <jj.mateer@live.com>
2019-07-05 13:28:23 -0700

    When button is clicked, the button adds it's value to the total score and displays it on the screen. However, all 4 buttons have the value of the 4th number.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,30 +13,32 @@ $("#randomNum").html('Random Number: ' + randomResult);
 
 // Every crystal needs to have a random number between 1 and 12
 for (var i = 0; i < 4; i++) {
-    var random = Math.floor(Math.random() *  12);
+    var random = Math.floor(Math.random() * 12) + 1;
     console.log(random);
+   
+    var crystal = $('<button>', '</button>');
 
-    var crystal = $('<button>');
     crystal.attr({
         "class": 'crystal',
-        "data-random": random
-        
     });
+
     $(".crystals").append(crystal);
-
-
+    $(".crystal").push(random);
+    
 }
 
-//When the player clicks on a crystal, it will add a specific amount of points to the plays total score.
-    $(".crystal").click(function(){
-        userScore = crystal + userScore
+//When the player clicks on a crystal, it will add a specific amount of points to the players total score.
+$('.crystal').on("click", function() {
 
-        $('#totalScore2').append(userScore);
+userScore =  random + userScore;
+   
 
-    });
-    
+$('#totalScore2').text(userScore)
+})
+
 
 //If the total score matches the random number, then the player wins.
+
 
 //The player loses if their score goes above the random number.
 
