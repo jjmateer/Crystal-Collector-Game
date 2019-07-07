c29e8eafbf1ea62312a9518ed0052ba95cac8500
John Mateer <jj.mateer@live.com>
2019-07-07 12:07:22 -0700

    took out the excessive calling of the reset function and took out the var tags in the reset function. The buttons now have random values after each win or loss.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,24 +11,28 @@ $(document).ready(function () {
     $("#randomNum").html('Random Number: ' + randomResult);
 
     // Make variables for the random value of each crystal
-
+  
     var num1 = Math.floor(Math.random() * 12 + 1);
     var num2 = Math.floor(Math.random() * 12 + 1);
     var num3 = Math.floor(Math.random() * 12 + 1);
     var num4 = Math.floor(Math.random() * 12 + 1);
-
+    
+    
     //Create a reset function
     function reset() {
         randomResult = Math.floor(Math.random() * 102 + 19);
         $("#randomNum").html('Random Number: ' + randomResult);
-        var num1 = Math.floor(Math.random() * 12 + 1);
-        var num2 = Math.floor(Math.random() * 12 + 1);
-        var num3 = Math.floor(Math.random() * 12 + 1);
-        var num4 = Math.floor(Math.random() * 12 + 1);
+         num1 = Math.floor(Math.random() * 12 + 1);
+         num2 = Math.floor(Math.random() * 12 + 1);
+         num3 = Math.floor(Math.random() * 12 + 1);
+         num4 = Math.floor(Math.random() * 12 + 1);
         userScore = 0;
-        console.log();
     }
-    // Show the number of games the player wins and loses and create functions to count wins and loses.
+
+
+    
+    reset();
+    // Create functions to count wins and loses and push them to the page.
     $('#winsText').text("Wins: " + wins);
     $('#lossesText').text("Losses: " + losses);
     function win() {
@@ -43,21 +47,22 @@ $(document).ready(function () {
         $('#lossesText').text("Losses: " + losses);
         reset();
     }
+    
     //Create on click functions for each crystal with if statements that determine if you win or lose and trigger a reset.
     $('#red').on("click", function () {
 
         userScore = num1 + userScore;
         if (userScore === randomResult) {
             win();
-            reset();
+            
 
         }
         if (userScore > randomResult) {
             lose();
-            reset();
+            
 
         }
-
+        
         $('#totalScore2').text(userScore)
     });
     $('#blue').on("click", function () {
@@ -65,12 +70,12 @@ $(document).ready(function () {
         userScore = num2 + userScore;
         if (userScore === randomResult) {
             win();
-            reset();
+            
 
         }
         if (userScore > randomResult) {
             lose();
-            reset();
+            
 
         }
 
@@ -81,12 +86,12 @@ $(document).ready(function () {
         userScore = num3 + userScore;
         if (userScore === randomResult) {
             win();
-            reset();
+            
 
         }
         if (userScore > randomResult) {
             lose();
-            reset();
+            
 
         }
 
@@ -97,16 +102,17 @@ $(document).ready(function () {
         userScore = num4 + userScore;
         if (userScore === randomResult) {
             win();
-            reset();
+            
 
         }
         if (userScore > randomResult) {
             lose();
-            reset();
+            
 
         }
 
 
         $('#totalScore2').text(userScore)
     });
+    
 });
