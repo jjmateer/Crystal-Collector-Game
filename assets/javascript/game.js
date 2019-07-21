6ff2ce6b0562f9f205d2f53900340960cf99b2ed
John Mateer <jj.mateer@live.com>
2019-07-20 21:01:36 -0700

    Revisited this repository to practice pseudo coding.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,57 +5,62 @@ $(document).ready(function () {
     var losses = 0;
     var userScore = 0;
 
-    //Create random number that you have to guess is between 19 and 120
-
+    //Create random number that you have to guess is between 19 and 120.
     randomResult = Math.floor(Math.random() * 102 + 19);
+    //Change the inner html of randomNum to the random number we generated.
     $("#randomNum").html('Random Number: ' + randomResult);
 
-    // Make variables for the random value of each crystal
-  
+    // Make variables for the random value of each crystal between 1 and 12.
     var num1 = Math.floor(Math.random() * 12 + 1);
     var num2 = Math.floor(Math.random() * 12 + 1);
     var num3 = Math.floor(Math.random() * 12 + 1);
     var num4 = Math.floor(Math.random() * 12 + 1);
-    
+
     //Create a reset function
     function reset() {
         randomResult = Math.floor(Math.random() * 102 + 19);
         $("#randomNum").html('Random Number: ' + randomResult);
-         num1 = Math.floor(Math.random() * 12 + 1);
-         num2 = Math.floor(Math.random() * 12 + 1);
-         num3 = Math.floor(Math.random() * 12 + 1);
-         num4 = Math.floor(Math.random() * 12 + 1);
+        num1 = Math.floor(Math.random() * 12 + 1);
+        num2 = Math.floor(Math.random() * 12 + 1);
+        num3 = Math.floor(Math.random() * 12 + 1);
+        num4 = Math.floor(Math.random() * 12 + 1);
         userScore = 0;
     }
-
     reset();
-    // Create functions to count wins and loses and push them to the page.
+
+    //Update the inner text of divs that hold wins and losses
     $('#winsText').text("Wins: " + wins);
     $('#lossesText').text("Losses: " + losses);
+    //Display text in the win area telling the user that they win, increment wins by 1, change 
+    //the inner text of the div for wins by displaying the newly updated win count.
     function win() {
         $('#win-area').text("You win!!!")
         wins++;
         $('#winsText').text("Wins: " + wins);
         reset();
     }
+    //Display text in the lose area telling the user that they lose, increment losses by 1, change 
+    //the inner text of the div for losses by displaying the newly updated lose count.
     function lose() {
         $('#win-area').text("You lose!!!")
         losses++;
         $('#lossesText').text("Losses: " + losses);
         reset();
     }
-    
-    //Create on click functions for each crystal with if statements that determine if you win or lose and trigger a reset.
+
+    //Each click will add the number value of the button to the users score.
+    //If the user score is equal to the random number that was generated, 
+    //call on the win function. If the score goes over the random number,
+    //call on the lose function.
     $('#red').on("click", function () {
 
         userScore = num1 + userScore;
         if (userScore === randomResult) {
             win();
-            }
-        if (userScore > randomResult) {
+        }else if (userScore > randomResult) {
             lose();
-            }
-        
+        }
+
         $('#totalScore2').text(userScore)
     });
     $('#blue').on("click", function () {
@@ -63,10 +68,9 @@ $(document).ready(function () {
         userScore = num2 + userScore;
         if (userScore === randomResult) {
             win();
-            }
-        if (userScore > randomResult) {
+        }else if (userScore > randomResult) {
             lose();
-            }
+        }
 
         $('#totalScore2').text(userScore)
     });
@@ -75,10 +79,9 @@ $(document).ready(function () {
         userScore = num3 + userScore;
         if (userScore === randomResult) {
             win();
-            }
-        if (userScore > randomResult) {
+        }else if (userScore > randomResult) {
             lose();
-            }
+        }
 
         $('#totalScore2').text(userScore)
     });
@@ -87,11 +90,10 @@ $(document).ready(function () {
         userScore = num4 + userScore;
         if (userScore === randomResult) {
             win();
-        }
-        if (userScore > randomResult) {
+        }else if (userScore > randomResult) {
             lose();
-            }
+        }
         $('#totalScore2').text(userScore)
     });
-    
+
 });
